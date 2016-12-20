421c7f7dcc3cefebd33c83099765c5e783ee4ac5
Jim Lott <mrjameslott@yahoo.co.uk>
2016-12-20 13:02:17 +0000

    change widget timing

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -8,30 +8,30 @@ chrome.extension.sendMessage({}, function(response) {
 		console.log("loading EKX widget extension");
 		// ----------------------------------------------------------
 	
-(function() {
-    'use strict';
-var QueryString = function () {
-  // This function is anonymous, is executed immediately and 
-  // the return value is assigned to QueryString!
-  var query_string = {};
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (var i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-        // If first entry with this name
-    if (typeof query_string[pair[0]] === "undefined") {
-      query_string[pair[0]] = decodeURIComponent(pair[1]);
-        // If second entry with this name
-    } else if (typeof query_string[pair[0]] === "string") {
-      var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
-      query_string[pair[0]] = arr;
-        // If third or later entry with this name
-    } else {
-      query_string[pair[0]].push(decodeURIComponent(pair[1]));
-    }
-  }
-  return query_string;
-}();
+// (function() {
+//     'use strict';
+// var QueryString = function () {
+//   // This function is anonymous, is executed immediately and 
+//   // the return value is assigned to QueryString!
+//   var query_string = {};
+//   var query = window.location.search.substring(1);
+//   var vars = query.split("&");
+//   for (var i=0;i<vars.length;i++) {
+//     var pair = vars[i].split("=");
+//         // If first entry with this name
+//     if (typeof query_string[pair[0]] === "undefined") {
+//       query_string[pair[0]] = decodeURIComponent(pair[1]);
+//         // If second entry with this name
+//     } else if (typeof query_string[pair[0]] === "string") {
+//       var arr = [ query_string[pair[0]],decodeURIComponent(pair[1]) ];
+//       query_string[pair[0]] = arr;
+//         // If third or later entry with this name
+//     } else {
+//       query_string[pair[0]].push(decodeURIComponent(pair[1]));
+//     }
+//   }
+//   return query_string;
+// }();
  
     // add div into side bar
     //if(!QueryString.EKXSHOW){ window.location.search = window.location.search + "&EKXSHOW=SHOW";}
@@ -39,27 +39,38 @@ var QueryString = function () {
   
 
   var script = document.createElement('script');
-  var head = document.getElementsByTagName('head')[0];
-  script.type = 'text/javascript';
-  script.src="https://host-ekx.rhcloud.com/controller.js";
-  head.appendChild(script);
- // script.onreadystatechange = loadWidget();
+  // var head = document.getElementsByTagName('head')[0];
   script.onload = loadWidget();
+  script.type = 'text/javascript';
+  document.body.appendChild(script);
+  //script.onreadystatechange = loadWidget();
+ 
+  script.src="https://host-ekx.rhcloud.com/controller.js";
 
-})();
+  // var script2 = document.createElement('script');
+  // script2.type = 'text/javascript';
+  // //  script.textContent=''
+  // document.body.appendChild(script2);
+  //   //script.onload = setWidget();
+  // script2.src=chrome.extension.getURL('src/inject/widgetscript.js');
 
-	}
-	}, 10);
+
+
+};
+
+	},10);
 });
 
 function loadWidget(){
-	  var script = document.createElement('script');
-	  var head = document.getElementsByTagName('head')[0];
+  var script = document.createElement('script');
+ 
+	  // var head = document.getElementsByTagName('head')[0];
 	  script.type = 'text/javascript';
 	//  script.textContent=''
-
-	  script.src=chrome.extension.getURL('src/inject/widgetscript.js');
-	  head.appendChild(script);
+    document.body.appendChild(script);
+    //script.onload = setWidget();
+    script.src=chrome.extension.getURL('src/inject/widgetscript.js');
+	 
 
 
 	// chrome.tabs.executeScript(null, {file: 'src/inject/widgetscript.js'});
--- ext/src/inject/widgetscript.js
+++ ext/src/inject/widgetscript.js
@@ -1,3 +1,7 @@
+console.log("loaded widget setting script")
+
+console.log(typeof ekxWidget)
+
 function addGlobalStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
@@ -8,11 +12,9 @@ function addGlobalStyle(css) {
     head.appendChild(style);
 }
 
-
-
- window.setTimeout(function(){
-
- 	var title ="";
+function setWidget(){
+	console.log("setting widget")
+	var title ="";
  	var index="";
     var container=[]
     var author="";
@@ -27,8 +29,8 @@ function addGlobalStyle(css) {
 	 			title =  document.getElementsByTagName("h1")[0].textContent
 	 			index = ['.tab-content'];
 	 			container= ['#EKX_mywidget'];
-                divloc="nav-tabs";
-	 			addGlobalStyle('#EKX_mywidget {position: fixed;right: 0;width: 100px;top: 150px; z-index:1000}');
+                divloc=["nav-tabs"];
+	 			addGlobalStyle('#EKX_mywidget {position: fixed;right: 0;width: 100px;top: 100px; z-index:1000}');
  			break;
  			case "ukerc.rl.ac.uk":
                 loadWidget = true
@@ -36,42 +38,48 @@ function addGlobalStyle(css) {
 	 			index = ['.resultsblock','.boundary-box'];
 	 			container= ['#EKX_mywidget'];
                 divloc=['resultsblock','boundary-box'];
-	 			addGlobalStyle('#EKX_mywidget {position: fixed;right: 0;width: 100px;top: 150px; }');
+	 			addGlobalStyle('#EKX_mywidget {position: fixed;right: 0;width: 100px;top: 100px; }');
  			break;
             case "www.ukerc.ac.uk":
                 loadWidget = true
                 title =  document.getElementsByTagName("h2")[0].textContent
                 index = ['#content'];
                 container= ['#EKX_mywidget'];
-                divloc="container"
+                divloc=["container"]
                 author=document.getElementsByTagName("h6")[0].textContent
-                addGlobalStyle('#EKX_mywidget {position: fixed;right: 0;width: 100px;top: 150px;z-index:1000 }');
+                addGlobalStyle('#EKX_mywidget {position: fixed;right: 0;width: 100px;top: 100px;z-index:1000 }');
             break;
             case "www.encraft.co.uk":
             break;
  	}
 
+		if(loadWidget){
+		    makeDiv(divloc)
 
 
-if(loadWidget){
-    makeDiv(divloc)
+		    var EKXWidget = new ekxWidget({
+		 				container : container,
+		 			//	follow : ['pdf'],
+		                index : index,
+		                title:title,
+		                author:author,
+		                show:show
+		            }) 
+		    }
+
+}
+
+ window.setTimeout(function(){
+	setWidget()
+},1000)
+
+//wait till EKX widget exists
 
 
-    var EKXWidget = new ekxWidget({
- 				container : container,
- 			//	follow : ['pdf'],
-                index : index,
-                title:title,
-                author:author,
-                show:show
-            }) 
-    }
-},2000)
-
-
+//window.onload(setWidget())
 
 function makeDiv(obj){
-    console.log(obj)
+    
      var e = document.createElement('div');
      e.id = 'EKX_mywidget';
      e.width ='600px';
@@ -79,13 +87,12 @@ function makeDiv(obj){
     if (obj.length==0){document.body.appendChild(e)}
     else {
    for (i=0;i<obj.length;i++){
+   	console.log(obj[i])
          if( document.getElementsByClassName(obj[i])[0]) {document.getElementsByClassName(obj[i])[0].appendChild(e)}
         }
     }
 
 }
-
-
 
 
 
